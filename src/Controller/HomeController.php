fe85df72981ab6d4807c67499d99df517e3b3ee2
Mamadou Bhoye Bah <bhoyemad11@gmail.com>
2024-11-22 20:14:58 +0000

    Tableau de board

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,8 +7,10 @@ use App\Entity\Encadreur;
 use App\Entity\Membres;
 use App\Entity\Reunion;
 use App\Entity\User;
+use App\Repository\ReunionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -16,16 +18,28 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class HomeController extends AbstractController
 {
     private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
+    private $reunionRepository;
+    public function __construct(EntityManagerInterface $entityManager,ReunionRepository $reunionRepository)
     {
         $this->entityManager = $entityManager;
+        $this->reunionRepository = $reunionRepository;
+
     }
+
+ 
+
 
     #[Route('/', name: 'app_home')]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        // Récupérer le mois et l'année depuis la requête (si présents)
+        $mois = $request->query->get('mois', date('m'));  // Utilise le mois courant par défaut
+        $annee = $request->query->get('annee', date('Y'));  // Utilise l'année courante par défaut
+        $date = \DateTime::createFromFormat('Y-m', "$annee-$mois");
+        // Récupérer le nombre de réunions par Dahira dans le mois spécifié
+        $resultats = $this->reunionRepository->countReunionsParDahira((int)$mois, (int)$annee);
+        // dd($resultats);
         // Compter les dahiras
           $membreCount = $this->entityManager->getRepository(Membres::class)->count([]);
           if($this->IsGranted('ROLE_USER')){
@@ -36,7 +50,13 @@ class HomeController extends AbstractController
           // Compter les encadreurs
           $encadreurCount = $this->entityManager->getRepository(Encadreur::class)->count([]);
           $reunionCount = $this->entityManager->getRepository(Reunion::class)->count([]);
-  
+            // Calculer le total des réunions pour ce mois
+            $totalReunions = 0;
+            foreach ($resultats as $resultat) {
+                $totalReunions += (int)$resultat['nombre_reunions'];  // Additionner le nombre de réunions
+            }
+          $ratio = "$totalReunions/$dahiraCount";
+            
           // Passer les comptages à la vue
           $data = [
               'userCount' => $userCount,
@@ -44,6 +64,11 @@ class HomeController extends AbstractController
               'dahiraCount' => $dahiraCount,
               'encadreurCount' => $encadreurCount,
               'reunionCount' => $reunionCount,
+              'resultats' => $resultats,
+              'mois' => $mois,
+              'annee' => $annee,
+              'ratio' => $ratio,
+              'moisLettre' => $date->format('F'),
           ];
 
         if ($this->isGranted('ROLE_ADMIN')) {
--- src/Repository/ReunionRepository.php
+++ src/Repository/ReunionRepository.php
@@ -16,6 +16,33 @@ class ReunionRepository extends ServiceEntityRepository
         parent::__construct($registry, Reunion::class);
     }
 
+    /**
+     * Récupérer le nombre de réunions par Dahira dans un mois donné
+     *
+     * @param int $mois Le mois (1 = janvier, 12 = décembre)
+     * @param int $annee L'année (ex : 2024)
+     * @return array Un tableau contenant le nombre de réunions par Dahira
+     */
+    public function countReunionsParDahira(int $mois, int $annee)
+    {
+        // Créer une date de début et de fin pour le mois donné
+        $dateDebut = new \DateTime("$annee-$mois-01 00:00:00");
+        $dateFin = clone $dateDebut;
+        $dateFin->modify('last day of this month')->setTime(23, 59, 59);
+
+        // Créer la requête DQL
+        $qb = $this->createQueryBuilder('r')
+            ->select('dahira.id AS dahira_id', 'COUNT(r.id) AS nombre_reunions')
+            ->innerJoin('r.dahiras', 'dahira')
+            ->where('r.date BETWEEN :dateDebut AND :dateFin')
+            ->groupBy('dahira.id')
+            ->setParameter('dateDebut', $dateDebut)
+            ->setParameter('dateFin', $dateFin);
+
+        // Exécuter la requête et retourner les résultats
+        return $qb->getQuery()->getResult();
+    }
+
     //    /**
     //     * @return Reunion[] Returns an array of Reunion objects
     //     */
