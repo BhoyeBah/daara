1eca2ce4624375a3d04d6835e124b3dee3b25f10
Mamadou Bhoye Bah <bhoyemad11@gmail.com>
2024-11-28 18:03:58 +0000

    form login image

--- var/cache/dev/twig/5e/5e48a3fec2260a3ac6920a88743bfaa4.php
+++ var/cache/dev/twig/5e/5e48a3fec2260a3ac6920a88743bfaa4.php
@@ -63,27 +63,35 @@ class __TwigTemplate_9a9f9bb32bf59bda12a2c8b62c23df43 extends Template
             justify-content: center;
             padding: 15px;
         }
-        .form-container {
+        .login-box {
             background: #fff;
             border-radius: 10px;
             box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-            padding: 30px;
+            display: flex;
+            flex-direction: row;
+            overflow: hidden;
+            max-width: 800px; /* Limite la largeur totale */
             width: 100%;
-            max-width: 400px;
+        }
+        .form-container {
+            padding: 20px;
+            width: 45%; /* Réduit la taille du formulaire */
         }
         .form-image {
+            width: 55%; /* Réduit la taille de l'image */
             display: flex;
             justify-content: center;
             align-items: center;
         }
         .form-image img {
-            max-width: 100%;
-            height: auto;
-            border-radius: 10px;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            align-items: center;
         }
         .logo-container img {
-            max-width: 120px;
-            margin-bottom: 20px;
+            max-width: 100px; /* Réduit la taille du logo */
+            margin-bottom: 15px;
         }
         .btn-success {
             background-color: #28a745;
@@ -101,72 +109,73 @@ class __TwigTemplate_9a9f9bb32bf59bda12a2c8b62c23df43 extends Template
 </head>
 <body>
     <div class=\"container login-container\">
-        <div class=\"row align-items-center justify-content-center\">
+        <div class=\"login-box\">
+            <!-- Image -->
+            <div class=\"form-image cover\">
+                <img src=\"";
+        // line 68
+        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape($this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("vendors/images/login-page-img.jpg"), "html", null, true);
+        yield "\" alt=\"Image\">
+            </div>
             <!-- Form -->
-            <div class=\"col-lg-6\">
-                <div class=\"form-container\">
-                    <div class=\"text-center logo-container\">
-                        <img src=\"";
-        // line 62
+            <div class=\"form-container\">
+                <div class=\"text-center logo-container\">
+                    <img src=\"";
+        // line 73
         yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape($this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("vendors/images/deskapp-logo.jpg"), "html", null, true);
         yield "\" alt=\"Logo\">
-                    </div>
-                    <h3 class=\"text-center text-success mb-4\">Se Connecter</h3>
-                    <form method=\"POST\">
-                        ";
-        // line 66
-        if ((isset($context["error"]) || array_key_exists("error", $context) ? $context["error"] : (function () { throw new RuntimeError('Variable "error" does not exist.', 66, $this->source); })())) {
-            // line 67
-            yield "                        <div class=\"alert alert-danger\">";
-            yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape($this->extensions['Symfony\Bridge\Twig\Extension\TranslationExtension']->trans(CoreExtension::getAttribute($this->env, $this->source, (isset($context["error"]) || array_key_exists("error", $context) ? $context["error"] : (function () { throw new RuntimeError('Variable "error" does not exist.', 67, $this->source); })()), "messageKey", [], "any", false, false, false, 67), CoreExtension::getAttribute($this->env, $this->source, (isset($context["error"]) || array_key_exists("error", $context) ? $context["error"] : (function () { throw new RuntimeError('Variable "error" does not exist.', 67, $this->source); })()), "messageData", [], "any", false, false, false, 67), "security"), "html", null, true);
+                </div>
+                <h3 class=\"text-center text-success mb-3\">Se Connecter</h3>
+                <form method=\"POST\">
+                    ";
+        // line 77
+        if ((isset($context["error"]) || array_key_exists("error", $context) ? $context["error"] : (function () { throw new RuntimeError('Variable "error" does not exist.', 77, $this->source); })())) {
+            // line 78
+            yield "                    <div class=\"alert alert-danger\">";
+            yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape($this->extensions['Symfony\Bridge\Twig\Extension\TranslationExtension']->trans(CoreExtension::getAttribute($this->env, $this->source, (isset($context["error"]) || array_key_exists("error", $context) ? $context["error"] : (function () { throw new RuntimeError('Variable "error" does not exist.', 78, $this->source); })()), "messageKey", [], "any", false, false, false, 78), CoreExtension::getAttribute($this->env, $this->source, (isset($context["error"]) || array_key_exists("error", $context) ? $context["error"] : (function () { throw new RuntimeError('Variable "error" does not exist.', 78, $this->source); })()), "messageData", [], "any", false, false, false, 78), "security"), "html", null, true);
             yield "</div>
-                        ";
+                    ";
         }
-        // line 69
-        yield "                        <div class=\"mb-3\">
-                            <label for=\"username\" class=\"form-label\">Email</label>
-                            <input type=\"email\" class=\"form-control\" id=\"username\" name=\"_username\" value=\"";
-        // line 71
-        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape((isset($context["last_username"]) || array_key_exists("last_username", $context) ? $context["last_username"] : (function () { throw new RuntimeError('Variable "last_username" does not exist.', 71, $this->source); })()), "html", null, true);
+        // line 80
+        yield "                    <div class=\"mb-3\">
+                        <label for=\"username\" class=\"form-label\">Email</label>
+                        <input type=\"email\" class=\"form-control\" id=\"username\" name=\"_username\" value=\"";
+        // line 82
+        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape((isset($context["last_username"]) || array_key_exists("last_username", $context) ? $context["last_username"] : (function () { throw new RuntimeError('Variable "last_username" does not exist.', 82, $this->source); })()), "html", null, true);
         yield "\" placeholder=\"Votre email\" required>
-                        </div>
-                        <div class=\"mb-3\">
-                            <label for=\"password\" class=\"form-label\">Mot de passe</label>
-                            <input type=\"password\" class=\"form-control\" id=\"password\" name=\"_password\" placeholder=\"********\" required>
-                        </div>
-                        <div class=\"mb-3 form-check\">
-                            <input type=\"checkbox\" class=\"form-check-input\" id=\"remember_me\" name=\"_remember_me\">
-                            <label class=\"form-check-label\" for=\"remember_me\">Se souvenir de moi</label>
-                        </div>
-                        <div class=\"d-flex justify-content-between mb-3\">
-                            <a href=\"forgot-password.html\" class=\"text-muted\">Mot de passe oublié ?</a>
-                        </div>
-                        <input type=\"hidden\" name=\"_csrf_token\" value=\"";
-        // line 84
+                    </div>
+                    <div class=\"mb-3\">
+                        <label for=\"password\" class=\"form-label\">Mot de passe</label>
+                        <input type=\"password\" class=\"form-control\" id=\"password\" name=\"_password\" placeholder=\"********\" required>
+                    </div>
+                    <div class=\"mb-3 form-check\">
+                        <input type=\"checkbox\" class=\"form-check-input\" id=\"remember_me\" name=\"_remember_me\">
+                        <label class=\"form-check-label\" for=\"remember_me\">Se souvenir de moi</label>
+                    </div>
+                    <div class=\"d-flex justify-content-between mb-3\">
+                        <a href=\"forgot-password.html\" class=\"text-muted\">Mot de passe oublié ?</a>
+                    </div>
+                    <input type=\"hidden\" name=\"_csrf_token\" value=\"";
+        // line 95
         yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape($this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderCsrfToken("authenticate"), "html", null, true);
         yield "\">
-                        <button type=\"submit\" class=\"btn btn-success btn-lg w-100\">Se connecter</button>
-\t\t\t\t\t\t
-\t\t\t\t\t\t";
-        // line 87
-        if ((Twig\Extension\CoreExtension::length($this->env->getCharset(), (isset($context["users"]) || array_key_exists("users", $context) ? $context["users"] : (function () { throw new RuntimeError('Variable "users" does not exist.', 87, $this->source); })())) == 0)) {
-            // line 88
-            yield "                        <div class=\"text-center mt-4\">
-                            <p class=\"mb-2\">Vous n'avez pas de compte ?</p>
-                            <a href=\"";
-            // line 90
+                    <button type=\"submit\" class=\"btn btn-success btn-lg w-100\">Se connecter</button>
+                    
+                    ";
+        // line 98
+        if ((Twig\Extension\CoreExtension::length($this->env->getCharset(), (isset($context["users"]) || array_key_exists("users", $context) ? $context["users"] : (function () { throw new RuntimeError('Variable "users" does not exist.', 98, $this->source); })())) == 0)) {
+            // line 99
+            yield "                    <div class=\"text-center mt-3\">
+                        <p class=\"mb-2\">Vous n'avez pas de compte ?</p>
+                        <a href=\"";
+            // line 101
             yield $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("app_register");
             yield "\" class=\"btn btn-outline-success btn-lg\">S'inscrire</a>
-                        </div>
-\t\t\t\t\t\t";
+                    </div>
+                    ";
         }
-        // line 93
-        yield "                    </form>
-                </div>
-            </div>
-            <!-- Image -->
-            <div class=\"col-lg-6 d-none d-lg-block form-image\">
-                <img src=\"vendors/images/login-page-img.png\" alt=\"Image\">
+        // line 104
+        yield "                </form>
             </div>
         </div>
     </div>
@@ -204,7 +213,7 @@ class __TwigTemplate_9a9f9bb32bf59bda12a2c8b62c23df43 extends Template
      */
     public function getDebugInfo(): array
     {
-        return array (  164 => 93,  158 => 90,  154 => 88,  152 => 87,  146 => 84,  130 => 71,  126 => 69,  120 => 67,  118 => 66,  111 => 62,  48 => 1,);
+        return array (  178 => 104,  172 => 101,  168 => 99,  166 => 98,  160 => 95,  144 => 82,  140 => 80,  134 => 78,  132 => 77,  125 => 73,  117 => 68,  48 => 1,);
     }
 
     public function getSourceContext(): Source
@@ -227,27 +236,35 @@ class __TwigTemplate_9a9f9bb32bf59bda12a2c8b62c23df43 extends Template
             justify-content: center;
             padding: 15px;
         }
-        .form-container {
+        .login-box {
             background: #fff;
             border-radius: 10px;
             box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-            padding: 30px;
+            display: flex;
+            flex-direction: row;
+            overflow: hidden;
+            max-width: 800px; /* Limite la largeur totale */
             width: 100%;
-            max-width: 400px;
+        }
+        .form-container {
+            padding: 20px;
+            width: 45%; /* Réduit la taille du formulaire */
         }
         .form-image {
+            width: 55%; /* Réduit la taille de l'image */
             display: flex;
             justify-content: center;
             align-items: center;
         }
         .form-image img {
-            max-width: 100%;
-            height: auto;
-            border-radius: 10px;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            align-items: center;
         }
         .logo-container img {
-            max-width: 120px;
-            margin-bottom: 20px;
+            max-width: 100px; /* Réduit la taille du logo */
+            margin-bottom: 15px;
         }
         .btn-success {
             background-color: #28a745;
@@ -265,48 +282,46 @@ class __TwigTemplate_9a9f9bb32bf59bda12a2c8b62c23df43 extends Template
 </head>
 <body>
     <div class=\"container login-container\">
-        <div class=\"row align-items-center justify-content-center\">
-            <!-- Form -->
-            <div class=\"col-lg-6\">
-                <div class=\"form-container\">
-                    <div class=\"text-center logo-container\">
-                        <img src=\"{{ asset('vendors/images/deskapp-logo.jpg') }}\" alt=\"Logo\">
-                    </div>
-                    <h3 class=\"text-center text-success mb-4\">Se Connecter</h3>
-                    <form method=\"POST\">
-                        {% if error %}
-                        <div class=\"alert alert-danger\">{{ error.messageKey|trans(error.messageData, 'security') }}</div>
-                        {% endif %}
-                        <div class=\"mb-3\">
-                            <label for=\"username\" class=\"form-label\">Email</label>
-                            <input type=\"email\" class=\"form-control\" id=\"username\" name=\"_username\" value=\"{{ last_username }}\" placeholder=\"Votre email\" required>
-                        </div>
-                        <div class=\"mb-3\">
-                            <label for=\"password\" class=\"form-label\">Mot de passe</label>
-                            <input type=\"password\" class=\"form-control\" id=\"password\" name=\"_password\" placeholder=\"********\" required>
-                        </div>
-                        <div class=\"mb-3 form-check\">
-                            <input type=\"checkbox\" class=\"form-check-input\" id=\"remember_me\" name=\"_remember_me\">
-                            <label class=\"form-check-label\" for=\"remember_me\">Se souvenir de moi</label>
-                        </div>
-                        <div class=\"d-flex justify-content-between mb-3\">
-                            <a href=\"forgot-password.html\" class=\"text-muted\">Mot de passe oublié ?</a>
-                        </div>
-                        <input type=\"hidden\" name=\"_csrf_token\" value=\"{{ csrf_token('authenticate') }}\">
-                        <button type=\"submit\" class=\"btn btn-success btn-lg w-100\">Se connecter</button>
-\t\t\t\t\t\t
-\t\t\t\t\t\t{% if users|length == 0 %}
-                        <div class=\"text-center mt-4\">
-                            <p class=\"mb-2\">Vous n'avez pas de compte ?</p>
-                            <a href=\"{{ path('app_register') }}\" class=\"btn btn-outline-success btn-lg\">S'inscrire</a>
-                        </div>
-\t\t\t\t\t\t{% endif %}
-                    </form>
-                </div>
-            </div>
+        <div class=\"login-box\">
             <!-- Image -->
-            <div class=\"col-lg-6 d-none d-lg-block form-image\">
-                <img src=\"vendors/images/login-page-img.png\" alt=\"Image\">
+            <div class=\"form-image cover\">
+                <img src=\"{{ asset('vendors/images/login-page-img.jpg') }}\" alt=\"Image\">
+            </div>
+            <!-- Form -->
+            <div class=\"form-container\">
+                <div class=\"text-center logo-container\">
+                    <img src=\"{{ asset('vendors/images/deskapp-logo.jpg') }}\" alt=\"Logo\">
+                </div>
+                <h3 class=\"text-center text-success mb-3\">Se Connecter</h3>
+                <form method=\"POST\">
+                    {% if error %}
+                    <div class=\"alert alert-danger\">{{ error.messageKey|trans(error.messageData, 'security') }}</div>
+                    {% endif %}
+                    <div class=\"mb-3\">
+                        <label for=\"username\" class=\"form-label\">Email</label>
+                        <input type=\"email\" class=\"form-control\" id=\"username\" name=\"_username\" value=\"{{ last_username }}\" placeholder=\"Votre email\" required>
+                    </div>
+                    <div class=\"mb-3\">
+                        <label for=\"password\" class=\"form-label\">Mot de passe</label>
+                        <input type=\"password\" class=\"form-control\" id=\"password\" name=\"_password\" placeholder=\"********\" required>
+                    </div>
+                    <div class=\"mb-3 form-check\">
+                        <input type=\"checkbox\" class=\"form-check-input\" id=\"remember_me\" name=\"_remember_me\">
+                        <label class=\"form-check-label\" for=\"remember_me\">Se souvenir de moi</label>
+                    </div>
+                    <div class=\"d-flex justify-content-between mb-3\">
+                        <a href=\"forgot-password.html\" class=\"text-muted\">Mot de passe oublié ?</a>
+                    </div>
+                    <input type=\"hidden\" name=\"_csrf_token\" value=\"{{ csrf_token('authenticate') }}\">
+                    <button type=\"submit\" class=\"btn btn-success btn-lg w-100\">Se connecter</button>
+                    
+                    {% if users|length == 0 %}
+                    <div class=\"text-center mt-3\">
+                        <p class=\"mb-2\">Vous n'avez pas de compte ?</p>
+                        <a href=\"{{ path('app_register') }}\" class=\"btn btn-outline-success btn-lg\">S'inscrire</a>
+                    </div>
+                    {% endif %}
+                </form>
             </div>
         </div>
     </div>
